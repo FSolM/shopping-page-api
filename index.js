81d5070d8cf963cc63c3f5290c63b1aeaea18432
Carlos Sol <fersolmarin@gmail.com>
2021-06-10 22:56:49 -0500

    Base Config Modified

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('API is running!')
 });
+
+require('./api/cartOperations')(app);
+require('./api/shirtOperations')(app);
+require('./api/penOperations')(app);
+require('./api/mugOperations')(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
