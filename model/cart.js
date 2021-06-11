f2dd12cfae3660faf473264403cf7f75660be4b1
Carlos Sol <fersolmarin@gmail.com>
2021-06-11 17:51:50 -0500

    cart model Modified

--- model/cart.js
+++ model/cart.js
@@ -1,7 +1,7 @@
-const cart = [
-  { "shirt": { quantity: 0, price: 2000 }},
-  { "pen": { quantity: 0, price: 500 }},
-  {"cup": { quantity: 0, price: 750 }}
-];
+const cart = {
+  shirt: { quantity: 0, price: 2000 },
+  pen: { quantity: 0, price: 500 },
+  mug: { quantity: 0, price: 750 }
+};
 
 exports.cart = cart;
